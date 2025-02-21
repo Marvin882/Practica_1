f2b0f21b6d8c35eb5e70585a6874592984bc256a
Marvin882 <6439059@clases.edu.sv>
2025-02-20 21:05:38 -0600

    Majonchos

---
+++ Majoncho 6439059/Majoncho 6439059/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Majoncho_6439059
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            double majonchocost;
+            int cantidadmajonchos;
+            double totalmajonchocost;
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            majonchocost = Convert.ToDouble(textBox3
+              .Text);
+            cantidadmajonchos = Convert.ToInt32(textBox1.Text);
+            totalmajonchocost = majonchocost * cantidadmajonchos;
+            textBox3.Text = "$" + Convert.ToInt32(totalmajonchocost);
+        }
+    }
+}
