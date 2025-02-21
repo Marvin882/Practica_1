591e4dfff42dd7d9be1330f88572e7a61b49f18c
Marvin882 <6439059@clases.edu.sv>
2025-02-20 21:05:02 -0600

    ejercicio 2.3

---
+++ Ejercicio2.3 6439059/Ejercicio2.3 6439059/Características.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._3_6439059
+{
+    internal class Características
+    {
+         
+        public Características(string marca, string procesador, string memoria, string discoDuro )
+        {
+            this.marca = marca;
+            this.procesador = procesador;
+            this.memoria = memoria;
+            this.discoDuro = discoDuro;
+        }
+        public string marca;
+        public string procesador;
+        public string memoria;
+        public string discoDuro;
+
+        public void mayorCapacidad(string MAYORcapacidad)
+        {
+            Console.WriteLine("la capacidad aumentará de 100GB a 1000GB");
+        }
+        public void MenorCapacidad(string MENORcapacidad)
+        {
+            Console.WriteLine("La capacidad se reduce a 100GB");
+        }
+
+    }
+}
---
+++ Ejercicio2.3 6439059/Ejercicio2.3 6439059/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._3_6439059
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Características Personal = new Características("DELL", "INTEL core 5", "130 GB", "SSD");
+            Console.WriteLine("descripcion de las caracteristicas del ordenador");
+            Console.WriteLine("La marca es: " + Personal.marca);
+            Console.WriteLine("El procesador es " + Personal.procesador);
+            Console.WriteLine("La memoria es de " + Personal.memoria);
+            Console.WriteLine("el disco duro es de " + Personal.discoDuro);
+
+            Personal.mayorCapacidad("");
+            Personal.MenorCapacidad("");
+            Console.ReadKey();
+            
+        }
+    }
+}
